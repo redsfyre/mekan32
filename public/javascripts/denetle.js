9379997734994b3b49721ea442ba09d4795f3d42
Yasin İsa YILDIRIM <yasinisayildirim@protonmail.com>
2021-01-16 13:58:35 +0300

    Update denetle.js

--- public/javascripts/denetle.js
+++ public/javascripts/denetle.js
@@ -1,17 +1,11 @@
-$("#yorumEkle").submit(function (e) {
-  $(".alert.alert-danger").hider();
-  if (
-    !$("input#name").val() ||
-    !$("select#rating").val() ||
-    !$("textarea#review").val()
-  ) {
-    if ($(".alert.alert-danger").length) {
-      $(".alert.alert-danger").show();
+$('#yorumEkle').submit(function (e){
+  $('.alert.alert-danger').hide();
+  if (!$('input#name').val() || !$('select#rating').val() || !$('textarea#review').val()){
+    if ($('.alert.alert-danger').length) {
+      $('.alert.alert-danger').show();
     } else {
-      $(this).prepend(
-        '<div role="alert" class="alert alert-danger">Tüm Alanlar Gereklidir!</div>'
-      );
-      return false;
+      $(this).prepend('<div role="alert" class="alert alert-danger">Tüm Alanlar Gereklidir!</div>');
     }
+    return false;
   }
 });
