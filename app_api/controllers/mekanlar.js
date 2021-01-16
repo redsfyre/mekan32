6715bfc128564a808380181fbba1c2462ca5c831
Yasin İsa YILDIRIM <yasinisayildirim@protonmail.com>
2021-01-16 14:44:48 +0300

    Ödev dosyaları hazırlandı v3

--- app_api/controllers/mekanlar.js
+++ app_api/controllers/mekanlar.js
@@ -24,6 +24,7 @@ const mekanlariListele = async(req, res) => {
     return;
   }
 
+<<<<<<< HEAD
   try {
     const sonuc = await Mekan.aggregate([{
       $geoNear: {
@@ -46,6 +47,46 @@ const mekanlariListele = async(req, res) => {
     console.log(e);
     cevapOlustur(res, 404, e);
   }
+=======
+	var nokta= {
+		type: "Point",
+		coordinates: [enlem,boylam]
+	};
+	var geoOptions = {
+		distanceField: "mesafe",
+		spherical: true,
+		key:"koordinatlar"
+	};
+	if (!enlem || !boylam){
+		cevapOlustur(res, 404, {
+			"mesaj": "enlem ve boylam zorunlu parametrelerdir"
+		});
+		return;
+	}
+		try {
+			const sonuc = await Mekan.aggregate([
+				{
+					$geoNear: {
+						near: nokta,
+						...geoOptions
+					}
+				}
+			]);
+			const mekanlar = sonuc.map(mekan=> { return {
+				_id: mekan._id,
+				ad: mekan.ad,
+				adres: mekan.adres,
+				puan: mekan.puan,
+				imkanlar: mekan.imkanlar,
+				mesafe: mekan.mesafe.toFixed()+'m',
+			}});
+			cevapOlustur (res, 200, mekanlar);
+		}
+		catch(e){
+			console.log(e);
+			cevapOlustur (res, 404, e);
+		}
+>>>>>>> parent of e9b2eed (Ödev dosyaları hazırlandı v1)
 }
 
 const mekanEkle = function(req, res){
@@ -149,6 +190,7 @@ const mekanSil = function(req, res){
   }
 }
 
+<<<<<<< HEAD
 module.exports = {
   mekanlariListele,
   mekanEkle,
@@ -156,3 +198,12 @@ module.exports = {
   mekanSil,
   mekanGuncelle
 }
+=======
+module.exports={
+	mekanlariListele,
+	mekanEkle,
+	mekanGetir,
+	mekanSil,
+	mekanGuncelle
+}
+>>>>>>> parent of e9b2eed (Ödev dosyaları hazırlandı v1)
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+<<<<<<< HEAD
 var ctrlMekanlar = require('../controllers/mekanlar');
 var ctrlDigerleri = require('../controllers/digerleri');
 
@@ -9,4 +10,13 @@ router.get('/mekan/:mekanid/yorum/yeni', ctrlMekanlar.yorumEkle);
 router.post('/mekan/:mekanid/yorum/yeni', ctrlMekanlar.yorumumuEkle);
 router.get('/hakkinda', ctrlDigerleri.hakkinda);
 
+=======
+var ctrlMekanlar=require('../controllers/mekanlar');
+var ctrlDigerleri=require('../controllers/digerleri');
+
+router.get('/', ctrlMekanlar.anaSayfa);
+router.get('/mekan', ctrlMekanlar.mekanBilgisi);
+router.get('/mekan/yorum/yeni', ctrlMekanlar.yorumEkle);
+router.get('/hakkinda', ctrlDigerleri.hakkinda);
+>>>>>>> parent of e9b2eed (Ödev dosyaları hazırlandı v1)
 module.exports = router;
